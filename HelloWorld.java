66f4f3541a389e940ae1d79764ca8cbb9a079749
TrinhNghi <523h0061@student.tdtu.edu.vn>
2025-05-03 22:23:44 +0700

    User1 add line to HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args) {
+        System.out.println("User1 added this line!");
         System.out.println("Modified by user1!");
         System.out.println("User2!");
     }
