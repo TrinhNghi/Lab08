6ad896864812677713c1c99552af839de96fe96c
KobaltzStudio <guidervirus7486@gmail.com>
2025-05-03 22:56:39 +0700

    User2 change line 3

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,7 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("User1 added this line!");
-        System.out.println("User1 changed this!");
+        System.out.println("User2 changed this!");
         System.out.println("User2!");
         System.out.println("User2 added this line!");
     }
